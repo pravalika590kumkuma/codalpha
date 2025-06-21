8c4cd334b7154d6e337fb40c77976819e32b4d73
pravalika590kumkuma <pravalikakumkuma777@gmail.com>
2025-06-21 15:13:44 +0530

    Update imagegallary.js

--- imagegallary.js
+++ imagegallary.js
@@ -18,7 +18,7 @@
       });
     });
 
-    // Lightbox Logic
+    
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
     const closeBtn = document.querySelector('.close');
